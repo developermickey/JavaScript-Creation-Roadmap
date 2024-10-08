3da228abb7d107fd615683783a11fd23172478cc
developermickey <mukesh.quazma@gmail.com>
2024-10-08 23:34:56 +0530

    variables

---
+++ Variables/variables.js
@@ -0,0 +1,13 @@
+//  keywrod identifer = value;
+
+var name = "Mukesh Pathak";
+var age = 28;
+var phoneNumber = 8989898989;
+var isOnline = true;
+
+// ES 6 let and const
+
+let firstName = "Mukesh";
+// let firstName = "Mukesh"; // not vaild not declare again same name variable using let
+
+const PI = 3.14; //
